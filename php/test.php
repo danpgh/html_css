532a1c1fd3b8243dbffda7735b5466969658e053
Dan Paraschiv <danp@aisb.ro>
2018-11-27 15:32:28 +0200

    cd

---
+++ php/test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Bine ati venit</h1>
+  <p>
+    <?php
+    if (is_numeric($_GET['a']) && is_numeric($_GET['b']))
+        echo $_GET['a'] + $_GET['b'];
+        else
+          echo "Cel putin un parametru nu este un numar";
+
+     ?>
+</p>
+  </body>
+</html>
