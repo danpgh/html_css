b5350b99694d640be684b696a6fb79537b544396
Dan Paraschiv <danp@aisb.ro>
2018-11-27 11:57:02 +0200

    wedf

---
+++ php/array.php
@@ -0,0 +1,17 @@
+<?php
+$arr1 = [
+  1 => 'Unu',
+  2 => 'Doi',
+  3 => 'Trei'
+];
+
+ $arr2 = [
+   4 => 'Patru',
+   5 => 'Cinci'
+ ];
+
+$arr3 = array_merge($arr1, $arr2);
+print_r($arr3);
+
+
+ ?>
---
+++ php/array1.php
@@ -0,0 +1,7 @@
+<?php
+ $a = 5;
+ $b = $a++;
+ $c = ++$a;
+ echo "$a $b  $c", PHP_EOL
+
+ ?>
