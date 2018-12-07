b8085bebc9fb6a465add52797425b25123fe1c9a
Dan Paraschiv <danp@aisb.ro>
2018-12-07 10:36:13 +0200

    nb

--- php/sir.php
+++ php/sir.php
@@ -1,8 +1,10 @@
 <?php
 $a = array();
-  $a[0] = array('id','fname','lname','email')
+  $a[0] = array('id','fname','lname','email');
   $a[1] = array(1,'Peter','Anderson','peter@gmail.com');
-  for($i=0; $i<count($arr); $i++){
-  	echo $a[$i][1]."<br>";
+  $a[2] = array(2, 'Jon','Miller','john@gmail.com');
+  $a[3] = array(3, 'Thomas','Swift','thomas@gmail.com');
+  for($i=0; $i < count($a); $i++){
+  	echo $a[$i][0] . " " . $a[$i][1] . " " . $a[$i][2] . " " . $a[$i][3] .PHP_EOL;
   }
  ?>
