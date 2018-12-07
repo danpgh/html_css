c57e8499f798ae73aa542c3029485f9534454855
Dan Paraschiv <danp@aisb.ro>
2018-12-07 10:24:47 +0200

    hy

---
+++ php/sir.php
@@ -0,0 +1,8 @@
+<?php
+$a = array();
+  $a[0] = array('id','fname','lname','email')
+  $a[1] = array(1,'Peter','Anderson','peter@gmail.com');
+  for($i=0; $i<count($arr); $i++){
+  	echo $a[$i][1]."<br>";
+  }
+ ?>
