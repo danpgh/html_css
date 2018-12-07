b66f07bd75ea9d4112dd446161a322559f024162
Dan Paraschiv <danp@aisb.ro>
2018-12-07 10:41:00 +0200

    vv

--- php/sir.php
+++ php/sir.php
@@ -4,7 +4,7 @@ $a = array();
   $a[1] = array(1,'Peter','Anderson','peter@gmail.com');
   $a[2] = array(2, 'Jon','Miller','john@gmail.com');
   $a[3] = array(3, 'Thomas','Swift','thomas@gmail.com');
-  for($i=0; $i < count($a); $i++){
-  	echo $a[$i][0] . " " . $a[$i][1] . " " . $a[$i][2] . " " . $a[$i][3] .PHP_EOL;
+  for($n=0; $n < count($a); $n++){
+  	echo $a[$n][0] . " " . $a[$n][1] . " " . $a[$n][2] . " " . $a[$n][3] .PHP_EOL;
   }
  ?>
--- php/teste.php
+++ php/teste.php
@@ -1,3 +1,11 @@
 <?php
-$arr=array(array(array(1))); 
+$cars = array(
+  array("BMW",'525',2015),
+  array("Audi",'A8',2014),
+  array("Fiat",'500L',2015)
+);
+
+foreach($cars as $car){
+  echo "Brand: " . $car[0] . ", model: " . $car[1] . ", age: " . $car[2] . PHP_EOL;
+}
  ?>
